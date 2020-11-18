47d288dc8cee72a0b2ff537ecc8a84ebb6f4022e
Madhan <madhanmanoj2017@gmail.com>
2020-11-18 15:20:25 +0530

    react-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
+import React from "react"
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    usename :"",
+    password : "",
+    users : [],
+    isLoggedIn : false
+  }
+
+
+
+  handleLogin = () =>{
+    let username = this.state.username
+    let password = this.state.password
+    
+    let body =  {
+      username : username,
+      password : password
+    }
+
+    axios.post("http://localhost:5000/login",body)
+      .then((response) => {
+        axios.get("http://localhost:5000/users")
+        .then((res) =>{
+          this.setState({users : res.data, isLoggedIn : true})
+        })
+        alert("success")
+      })
+      .catch((error => {
+        alert("failure")
+      }))
+  }
+
+  render(){
+
+    return (
+      <div className="App" style={{display : "flex", flexDirection : "column", width : 200}}>
+        <TextField style={{marginLeft : 30, marginBottom : 30}} id="standard-basic" label="Username" value={this.state.username}  onChange={(e) =>this.setState({username : e.target.value})} />
+        <TextField style={{marginLeft : 30, marginBottom : 30}} id="standard-basic"  type="password" label="Password" value={this.state.password} onChange={(e) =>this.setState({password : e.target.value})}/>
+         <Button style={{marginLeft : 30, marginBottom : 30, backgroundColor : "green"}} variant="contained" color="primary" onClick={this.handleLogin}>
+          Login
+        </Button>
+
+          {this.state.isLoggedIn &&
+            this.state.users.map(user => {
+                return <div>
+                    <p>{"name : " +user.name}</p>
+                    <p>{"age  : " +user.age}</p>
+                  </div>
+                })
+               }
+      </div>
+    );
+  }
 }
 
 export default App;
